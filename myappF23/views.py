8e9ecf92a11b64f94cf1e86deb5f5ee6ce64c32b
srujanoza <sonuoza24@gmail.com>
2023-11-05 22:58:16 -0500

    Template Inheritance

--- myappF23/views.py
+++ myappF23/views.py
@@ -9,15 +9,15 @@ def index(request):
         'category_list': category_list,
         'course_list': course_list,
     }
-    return render(request, 'myappF23/index0.html', context)
+    return render(request, 'myappF23/index.html', context)
 
 
 def about(request):
-    return render(request, 'myappF23/about0.html')
+    return render(request, 'myappF23/about.html')
 
 
 def detail(request, category_no):
     category = get_object_or_404(Category, pk=category_no)
     course_list = Course.objects.filter(categories=category)
-    return render(request, 'myappF23/details0.html', {'category': category, 'course_list': course_list})
+    return render(request, 'myappF23/details.html', {'category': category, 'course_list': course_list})
 
