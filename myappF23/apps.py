30c044941bfb6a34dadb930fcac8923ccc89a7e8
praj31 <rajputpraj.31@gmail.com>
2023-11-08 12:02:25 -0500

    removed original folder

--- myappF23/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Myappf23Config(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "myappF23"
--- myappF23/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'myappF23'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('category/<int:category_no>/', views.detail, name='detail'),
-]
